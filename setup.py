ca72ff1ead223174fdf65bb98566bafe17300988
amandakarolline <amandakarolline@hotmail.com>
2022-04-03 19:59:49 -0300

    Alteração setup.py

--- setup.py
+++ setup.py
@@ -63,9 +63,8 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                    "Directory %s ignored by pattern %s"
-                                    % (fn, pattern))
+                            print("Directory %s ignored by pattern %s" %
+                                  (fn, pattern))
                         break
                 if bad_name:
                     continue
@@ -86,9 +85,8 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                    "File %s ignored by pattern %s"
-                                    % (fn, pattern))
+                            print("File %s ignored by pattern %s" %
+                                  (fn, pattern))
                         break
                 if bad_name:
                     continue
