4b760bcd9933194e307fa5c5c87ec83795703484
amandakarolline <amandakarolline@hotmail.com>
2022-04-04 00:27:05 -0300

    Inclusão de teste no github_api.py

---
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -0,0 +1,14 @@
+from unittest.mock import Mock
+
+from libpythonpro import github_api
+
+
+def test_buscar_avatar():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {
+        'login': 'amandakarolline', 'id': 29805670,
+        'avatar_url': 'https://avatars.githubusercontent.com/u/29805670?v=4'
+    }
+    github_api.requests.get = Mock(return_value=resp_mock)
+    url = github_api.buscar_avatar('amandakarolline')
+    assert 'https://avatars.githubusercontent.com/u/29805670?v=4' == url
