de447a6fc256802b66f36f8b40b86e6aa961cee8
amandakarolline <amandakarolline@hotmail.com>
2022-04-04 00:09:43 -0300

    Atualização dos testes para utilização da biblioteca Mock.

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Amanda')
+    usuario = Usuario(nome='Amanda', email='amandakarolline@hotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Amanda'), Usuario(nome='Renzo')]
+    usuarios = [
+        Usuario(nome='Amanda', email='amandakarolline@hotmail.com'),
+        Usuario(nome='Renzo', email='amandakarolline@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
